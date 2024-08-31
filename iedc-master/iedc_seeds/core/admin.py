7df32377b1b913768ddd2755c574abd4ae2bbeba
Vishnu S <141705372+Vishnu252005@users.noreply.github.com>
2024-08-31 19:57:56 +0530

    Delete iedc-master/iedc_seeds directory

--- iedc-master/iedc_seeds/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from core.models import Registrant
-
-@admin.register(Registrant)
-class AuthorAdmin(admin.ModelAdmin):
-	list_display = ['name']
--- iedc-master/iedc_seeds/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home-page/', views.home_page,name='home-page'),
-    path('registration/', views.registration_view, name='registration'),
-]
